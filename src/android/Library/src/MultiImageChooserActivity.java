8adefd941ab8c45906b6d1eb7e288693bd059f70
veronicatc <48568658+veronicatc@users.noreply.github.com>
2020-07-28 14:16:24 -0300

    Adding import for FileChannel

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -36,7 +36,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.nio.channels;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
