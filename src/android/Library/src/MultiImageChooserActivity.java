89f7a58dba6c3dce6df5b0506570dcd644b18e22
veronicatc <48568658+veronicatc@users.noreply.github.com>
2020-07-28 14:01:14 -0300

    Added required import

--- src/android/Library/src/MultiImageChooserActivity.java
+++ src/android/Library/src/MultiImageChooserActivity.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.channels;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
