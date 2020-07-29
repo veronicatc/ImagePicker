8b405deee09b80ebc527a7aec4d9a33a42b0846a
veronicatc <48568658+veronicatc@users.noreply.github.com>
2020-07-28 23:07:37 -0300

    Comment use original changes, throw errors

--- src/android/com/synconset/ImagePicker/ImagePicker.java
+++ src/android/com/synconset/ImagePicker/ImagePicker.java
@@ -52,7 +52,7 @@ public class ImagePicker extends CordovaPlugin {
             int desiredHeight = 0;
             int quality = 100;
             int outputType = 0;
-            boolean useOriginal = true;
+            //boolean useOriginal = true;
             if (params.has("maximumImagesCount")) {
                 max = params.getInt("maximumImagesCount");
             }
@@ -68,15 +68,15 @@ public class ImagePicker extends CordovaPlugin {
             if (params.has("outputType")) {
                 outputType = params.getInt("outputType");
             }
-            if(params.has("use_original")){
+            /*if(params.has("use_original")){
                 useOriginal = params.getBoolean("use_original");
-            }
+            }*/
             imagePickerIntent.putExtra("MAX_IMAGES", max);
             imagePickerIntent.putExtra("WIDTH", desiredWidth);
             imagePickerIntent.putExtra("HEIGHT", desiredHeight);
             imagePickerIntent.putExtra("QUALITY", quality);
             imagePickerIntent.putExtra("OUTPUT_TYPE", outputType);
-            imagePickerIntent.putExtra("USE_ORIGINAL", useOriginal);
+            //imagePickerIntent.putExtra("USE_ORIGINAL", useOriginal);
             
             // some day, when everybody uses a cordova version supporting 'hasPermission', enable this:
             /*
